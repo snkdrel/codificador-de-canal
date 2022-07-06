7ef24e3f6024ad2b84d70cee0116511c4d330750
snkdrel <ces.zop@gmail.com>
2022-07-06 13:30:56 -0500

    Agrega código selector
    Agrega nuevo archivo con plantilla para el código general que elegirá
    cuál código aplicar.
    
    Nuevos commits se escribirán en inglés.

---
+++ codificador-vivado/codificador.srcs/sources_1/new/selector.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity selector is
+  Port ( secuencia_selectora : in std_logic_vector(2 downto 0) -- elige cual codigo aplicar
+        
+    );
+end selector;
+
+architecture Behavioral of selector is
+
+begin
+
+
+end Behavioral;
